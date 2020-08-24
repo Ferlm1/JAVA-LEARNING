adf90655c78ea353623931f54ed94c61c145e658
FerL <ferrocklazme@gmail.com>
2020-08-24 18:02:47 -0500

    Feature: new methods on CRacional and CAritmetica test

--- JAVA_LIBRO_CEBALLOS/CAP-3/NumeroRacional/Racional.java
+++ JAVA_LIBRO_CEBALLOS/CAP-3/NumeroRacional/Racional.java
@@ -1,8 +1,11 @@
 class Racional{
     public static void main(String[] args){
         //Punto de entrada a la aplicación
-        CRacional r1 = new CRacional(); // crear un objeto CRacional
-        r1.AsignarDatos(2, 5);
-        r1.VisualizarRacional(); 
+        CRacional r1 = new CRacional(6, 5); // crear un objeto CRacional
+        CRacional r2 = new CRacional(3, 5);
+        r1.Sumar(r2); 
+        r1.Restar(r2);
+        r1.Mutiplicar(r2);
+        r1.VisualizarRacional();
     }
 }
